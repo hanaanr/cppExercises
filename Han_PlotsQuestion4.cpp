e42ae1b2b8e26b0141b2f0911c6523e67ddb3e90
Hanaan <hrs363@nyu.edu>
2022-01-04 12:23:38 +0000

    Updated to add plot titles and axis labels

--- Han_PlotsQuestion4.cpp
+++ Han_PlotsQuestion4.cpp
@@ -6,8 +6,9 @@
 #include "pbPlots.cpp"
 using namespace std;
 
-int main()
-{   
+int main(){
+    
+    // vectors to save x, y plotting data using "outputYearData.txt"
     vector<double> yearX;
     vector<double> yearY1, yearY2;
     int year_x;
@@ -21,6 +22,7 @@ int main()
 
     }
 
+    // vectors to save x, y plotting data using "outputMonthData.txt"
     vector<double> monthX;
     vector<double> monthY1, monthY2;
     int month_x;
@@ -34,32 +36,104 @@ int main()
 
     }
 
-    RGBABitmapImageReference *imageRef1 = CreateRGBABitmapImageReference();
-    RGBABitmapImageReference *imageRef2 = CreateRGBABitmapImageReference();
-    RGBABitmapImageReference *imageRef3 = CreateRGBABitmapImageReference();
-    RGBABitmapImageReference *imageRef4 = CreateRGBABitmapImageReference();
-    
-    DrawScatterPlot(imageRef1, 600, 400, &yearX, &yearY1);
-    DrawScatterPlot(imageRef2, 600, 400, &yearX, &yearY2);
-    DrawScatterPlot(imageRef3, 600, 400, &monthX, &monthY1);
-    DrawScatterPlot(imageRef4, 600, 400, &monthX, &monthY2);
+    // for 4 plots
+	RGBABitmapImageReference *imageReference1 = CreateRGBABitmapImageReference();
+    RGBABitmapImageReference *imageReference2 = CreateRGBABitmapImageReference();
+    RGBABitmapImageReference *imageReference3 = CreateRGBABitmapImageReference();
+    RGBABitmapImageReference *imageReference4 = CreateRGBABitmapImageReference();
 
-    vector<double> *pngData1 = ConvertToPNG(imageRef1->image);
-    WriteToFile(pngData1, "yearPlotMax.png");
-    DeleteImage(imageRef1->image); 
+	ScatterPlotSeries *series1 = GetDefaultScatterPlotSeriesSettings();
+	series1->xs = &yearX;
+	series1->ys = &yearY1;
+	series1->linearInterpolation = true;
+	series1->lineType = toVector(L"solid");
+	series1->lineThickness = 1;
+	series1->color = GetBlack();
 
-    vector<double> *pngData2 = ConvertToPNG(imageRef2->image);
-    WriteToFile(pngData2, "yearPlotMin.png");
-    DeleteImage(imageRef2->image); 
+	ScatterPlotSettings *settings1 = GetDefaultScatterPlotSettings();
+	settings1->width = 600;
+	settings1->height = 400;
+	settings1->autoBoundaries = true;
+	settings1->autoPadding = true;
+	settings1->title = toVector(L"Max Temp vs Year");
+	settings1->xLabel = toVector(L"Max Temp");
+	settings1->yLabel = toVector(L"Year");
+	settings1->scatterPlotSeries->push_back(series1);
 
-    vector<double> *pngData3 = ConvertToPNG(imageRef3->image);
-    WriteToFile(pngData3, "monthPlotMax.png");
-    DeleteImage(imageRef3->image); 
+	ScatterPlotSeries *series2 = GetDefaultScatterPlotSeriesSettings();
+	series2->xs = &yearX;
+	series2->ys = &yearY2;
+	series2->linearInterpolation = true;
+	series2->lineType = toVector(L"solid");
+	series2->lineThickness = 1;
+	series2->color = GetBlack();
 
-    vector<double> *pngData4 = ConvertToPNG(imageRef4->image);
-    WriteToFile(pngData4, "monthPlotMin.png");
-    DeleteImage(imageRef4->image); 
+	ScatterPlotSettings *settings2 = GetDefaultScatterPlotSettings();
+	settings2->width = 600;
+	settings2->height = 400;
+	settings2->autoBoundaries = true;
+	settings2->autoPadding = true;
+	settings2->title = toVector(L"Min Temp vs Year");
+	settings2->xLabel = toVector(L"Min Temp");
+	settings2->yLabel = toVector(L"Year");
+	settings2->scatterPlotSeries->push_back(series2);
 
-    return 0;
+	ScatterPlotSeries *series3 = GetDefaultScatterPlotSeriesSettings();
+	series3->xs = &monthX;
+	series3->ys = &monthY1;
+	series3->linearInterpolation = true;
+	series3->lineType = toVector(L"solid");
+	series3->lineThickness = 1;
+	series3->color = GetBlack();
+
+	ScatterPlotSettings *settings3 = GetDefaultScatterPlotSettings();
+	settings3->width = 600;
+	settings3->height = 400;
+	settings3->autoBoundaries = true;
+	settings3->autoPadding = true;
+	settings3->title = toVector(L"Max Temp vs Month");
+	settings3->xLabel = toVector(L"Max Temp");
+	settings3->yLabel = toVector(L"Month");
+	settings3->scatterPlotSeries->push_back(series3);
+
+	ScatterPlotSeries *series4 = GetDefaultScatterPlotSeriesSettings();
+	series4->xs = &monthX;
+	series4->ys = &monthY2;
+	series4->linearInterpolation = true;
+	series4->lineType = toVector(L"solid");
+	series4->lineThickness = 1;
+	series4->color = GetBlack();
+
+	ScatterPlotSettings *settings4 = GetDefaultScatterPlotSettings();
+	settings4->width = 600;
+	settings4->height = 400;
+	settings4->autoBoundaries = true;
+	settings4->autoPadding = true;
+	settings4->title = toVector(L"Min Temp vs Month");
+	settings4->xLabel = toVector(L"Min Temp");
+	settings4->yLabel = toVector(L"Month");
+	settings4->scatterPlotSeries->push_back(series4);
+
+	DrawScatterPlotFromSettings(imageReference1, settings1);
+    DrawScatterPlotFromSettings(imageReference2, settings2);
+    DrawScatterPlotFromSettings(imageReference3, settings3);
+    DrawScatterPlotFromSettings(imageReference4, settings4);
+
+	vector<double> *pngdata1 = ConvertToPNG(imageReference1->image);
+	WriteToFile(pngdata1, "YearPlotMax.png");
+	DeleteImage(imageReference1->image);
+
+	vector<double> *pngdata2 = ConvertToPNG(imageReference2->image);
+	WriteToFile(pngdata2, "YearPlotMin.png");
+	DeleteImage(imageReference2->image);
+
+	vector<double> *pngdata3 = ConvertToPNG(imageReference3->image);
+	WriteToFile(pngdata3, "MonthPlotMax.png");
+	DeleteImage(imageReference3->image);
+
+	vector<double> *pngdata4 = ConvertToPNG(imageReference4->image);
+	WriteToFile(pngdata4, "MonthPlotMin.png");
+	DeleteImage(imageReference4->image);
+
+	return 0;
 }
-
